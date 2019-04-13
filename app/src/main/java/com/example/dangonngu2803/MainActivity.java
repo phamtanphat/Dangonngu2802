d1a423d85a1d10f33b4321302e7c55b42204c7a0
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-04-13 19:20:50 +0700

    a

--- app/src/main/java/com/example/dangonngu2803/MainActivity.java
+++ app/src/main/java/com/example/dangonngu2803/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.dangonngu2803;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtTk,edtMk;
+    Button btnDangnhap;
+    TextView txtNoidung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtMk = findViewById(R.id.edittextPassword);
+        edtTk = findViewById(R.id.edittextTaikhoan);
+        btnDangnhap = findViewById(R.id.buttonDangnhap);
+        txtNoidung = findViewById(R.id.textNoidung);
+
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taikhoan = edtTk.getText().toString();
+                String matkhau = edtMk.getText().toString();
+
+                txtNoidung.setText("Tai khoan : " + taikhoan + "\n" + "Mật khẩu : " + matkhau);
+            }
+        });
     }
 }
